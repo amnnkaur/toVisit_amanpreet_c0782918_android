a338918c00deb268da8cc2947ee062806ae1436f
Aman Kaur <amankaur2628@gmail.com>
2020-07-16 14:20:00 -0400

    Favourite List Adapter created for recycler view

---
+++ app/src/main/java/com/lambton/tovisit_amanpreet_c0782918_android/adapter/FavListAdapter.java
@@ -0,0 +1,35 @@
+package com.lambton.tovisit_amanpreet_c0782918_android.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FavListAdapter extends RecyclerView.Adapter<FavListAdapter.ListViewHolder> {
+
+    public FavListAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
