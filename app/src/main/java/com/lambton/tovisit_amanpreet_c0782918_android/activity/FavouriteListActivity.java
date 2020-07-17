29ce756c76400edc262c5969b7775ea972203e79
Aman Kaur <amankaur2628@gmail.com>
2020-07-17 16:17:22 -0400

    Adapter created for recycler view and added to the activity

--- app/src/main/java/com/lambton/tovisit_amanpreet_c0782918_android/activity/FavouriteListActivity.java
+++ app/src/main/java/com/lambton/tovisit_amanpreet_c0782918_android/activity/FavouriteListActivity.java
@@ -1,25 +1,96 @@
 package com.lambton.tovisit_amanpreet_c0782918_android.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.lambton.tovisit_amanpreet_c0782918_android.R;
+import com.lambton.tovisit_amanpreet_c0782918_android.adapter.FavListAdapter;
+import com.lambton.tovisit_amanpreet_c0782918_android.database.FavPlace;
+import com.lambton.tovisit_amanpreet_c0782918_android.database.FavPlaceRoomDB;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavouriteListActivity extends AppCompatActivity {
 
     RecyclerView rvFavList;
 
+    private FavPlaceRoomDB favPlaceRoomDB;
+    FavListAdapter favListAdapter;
+
+    List<FavPlace> favPlaceList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fav_list);
 
         rvFavList = findViewById(R.id.rvFavList);
         rvFavList.setLayoutManager(new LinearLayoutManager(this));
+        rvFavList.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL));
+        rvFavList.setAdapter(favListAdapter);
 
+        favPlaceList = new ArrayList<>();
+        favPlaceRoomDB = favPlaceRoomDB.getINSTANCE(this);
+
+        loadPlaces();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.btnSearch);
+
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                favListAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.btnAdd){
+
+            Intent intent = new Intent(FavouriteListActivity.this, MainActivity.class);
+            startActivity(intent);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadPlaces() {
+        favPlaceList = favPlaceRoomDB.favPlaceDao().getAllPlaces();
+
+        favListAdapter = new FavListAdapter(this,R.layout.item_place, favPlaceList);
+        rvFavList.setAdapter(favListAdapter);
 
     }
 }
--- app/src/main/java/com/lambton/tovisit_amanpreet_c0782918_android/adapter/FavListAdapter.java
+++ app/src/main/java/com/lambton/tovisit_amanpreet_c0782918_android/adapter/FavListAdapter.java
@@ -1,35 +1,86 @@
 package com.lambton.tovisit_amanpreet_c0782918_android.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lambton.tovisit_amanpreet_c0782918_android.R;
+import com.lambton.tovisit_amanpreet_c0782918_android.activity.MainActivity;
+import com.lambton.tovisit_amanpreet_c0782918_android.database.FavPlace;
+import com.lambton.tovisit_amanpreet_c0782918_android.database.FavPlaceRoomDB;
+
+import java.util.List;
+
 public class FavListAdapter extends RecyclerView.Adapter<FavListAdapter.ListViewHolder> {
 
-    public FavListAdapter() {
+    private static final String TAG = "PlaceAdapter";
+    Context context;
+    int layoutRes;
+    List<FavPlace> favPlaceList;
+
+    FavPlaceRoomDB favPlaceRoomDB;
+    MainActivity mainActivity;
+
+    public FavListAdapter(Context context, int resource, List<FavPlace> favPlaces) {
+
+        this.favPlaceList = favPlaces;
+        this.layoutRes = resource;
+        this.context = context;
+
+        favPlaceRoomDB = favPlaceRoomDB.getINSTANCE(context);
+
     }
 
     @NonNull
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        final LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(layoutRes, null);
+
+        ListViewHolder listViewHolder = new FavListAdapter.ListViewHolder(view);
+
+        return listViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
 
+        final FavPlace favPlace = favPlaceList.get(position);
+        holder.address.setText(favPlace.getAddress());
+        holder.latitude.setText(favPlace.getLatitude());
+        holder.longitude.setText(favPlace.getLongitude());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return favPlaceList.size();
+    }
+
+    private void loadPlaces() {
+        favPlaceList= favPlaceRoomDB.favPlaceDao().getAllPlaces();
+        notifyDataSetChanged();
+
     }
 
     public class ListViewHolder extends RecyclerView.ViewHolder {
+
+        TextView address;
+        TextView latitude;
+        TextView longitude;
+
         public ListViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            address = itemView.findViewById(R.id.tv_address);
+            latitude = itemView.findViewById(R.id.tv_latitude);
+            longitude = itemView.findViewById(R.id.tv_longitude);
         }
     }
 }
